22d6ab7a04bb60c7a8ea89cea7aa7fed5e5072bd
Lucas Li <ylilp@connect.ust.hk>
2024-10-18 12:41:15 +0800

    created files for Report entity

---
+++ backend/src/main/java/com/jpmorgan/codeforgood/entity/Report.java
@@ -0,0 +1,32 @@
+package com.jpmorgan.codeforgood.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "report")
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String imgUrl; // Image URL
+
+    private String location; // Location of the report
+
+    private String name; // Name of the reporter
+
+    private String phoneNumber; // Phone number of the reporter
+
+    private String email; // Email of the reporter
+
+    private String description; // Description of the report
+
+    private String pictureURL; // Another picture URL (could be an additional image for the report)
+
+    private int donationAmount;
+}
