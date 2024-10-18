960d3916939be1f00477dc49b99a9fcabae9eafc
Lucas Li <ylilp@connect.ust.hk>
2024-10-18 13:52:05 +0800

    small fix

--- backend/src/main/java/com/jpmorgan/codeforgood/controller/ReportController.java
+++ backend/src/main/java/com/jpmorgan/codeforgood/controller/ReportController.java
@@ -3,13 +3,11 @@ package com.jpmorgan.codeforgood.controller;
 import com.jpmorgan.codeforgood.controller.request.ReportRequest;
 import com.jpmorgan.codeforgood.controller.response.CreateReportResponse;
 import com.jpmorgan.codeforgood.controller.response.ReportResponse;
-import com.jpmorgan.codeforgood.controller.response.ReportResponse;
 import com.jpmorgan.codeforgood.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -20,11 +18,11 @@ import java.util.List;
 public class ReportController {
     private final ReportService reportService;
 
-    @GetMapping("")
+    @GetMapping("get")
     public ResponseEntity<List<ReportResponse>> getAllReports() {
         return ResponseEntity.ok(reportService.getAllReports());
     }
-    @PostMapping("")
+    @PostMapping("insert")
     public ResponseEntity<CreateReportResponse> createReport(@RequestBody ReportRequest request) {
         CreateReportResponse response = reportService.createReport(request);
         return ResponseEntity.ok(response);
