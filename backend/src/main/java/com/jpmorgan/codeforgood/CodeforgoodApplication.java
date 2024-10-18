4b9bb4d7927c02665312530859e888aad9877e8a
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2024-10-18 11:18:45 +0800

    Add backend

---
+++ backend/src/main/java/com/jpmorgan/codeforgood/CodeforgoodApplication.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.codeforgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeforgoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeforgoodApplication.class, args);
+	}
+
+}
